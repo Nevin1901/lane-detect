80749f634acbf9833ee0ea62c97852d96373aa94
Nevin <nevinpuri1901@gmail.com>
2022-06-06 18:15:53 +0200

    finalized hsv amounts

--- main.py
+++ main.py
@@ -1,43 +1,45 @@
 import numpy as np
+import time
 import cv2 as cv
 from mss import mss
 
 if __name__ == "__main__":
+    while True:
+        with mss() as sct:
+            monitor = {
+            "top": 30,
+            "left": 350,
+            "width": 1240,
+            "height": 918,
+            }
 
-    with mss() as sct:
-        monitor = {
-        "top": 30,
-        "left": 350,
-        "width": 1240,
-        "height": 918,
-        }
+            # 342
 
-        # 342
+            img = np.asarray(sct.grab(monitor))
+            gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+            dark = (gray * 0.5).astype(np.uint8)
 
-        img = np.asarray(sct.grab(monitor))
-        gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-        dark = (gray * 0.5).astype(np.uint8)
+            hsv = cv.cvtColor(img, cv.COLOR_RGB2HSV)
 
-        hsv = cv.cvtColor(img, cv.COLOR_RGB2HSV)
+            y_lower = np.array([80, 120, 80])
+            y_upper = np.array([200, 255, 255])
+            y_line = cv.inRange(hsv, y_lower, y_upper)
 
-        y_lower = np.array([40, 100, 50])
-        y_upper = np.array([255, 255, 255])
-        y_line = cv.inRange(hsv, y_lower, y_upper)
+            w_lower = np.array([0, 0, 200])
+            w_upper = np.array([255, 32, 255])
+            w_line = cv.inRange(hsv, w_lower, w_upper)
 
-        w_lower = np.array([0, 0, 240])
-        w_upper = np.array([255, 5, 255])
-        w_line = cv.inRange(hsv, w_lower, w_upper)
+            # combine two masks
+            combined = y_line | w_line
 
-        # combine two masks
-        combined = y_line | w_line
+            blur = cv.blur(combined, (5, 5))
 
-        blur = cv.blur(combined, (5, 5))
+            edges = cv.Canny(combined, 100, 200)
 
-        edges = cv.Canny(combined, 100, 200)
+            cv.imshow("frame", edges)
+            if cv.waitKey(1) & 0xFF == ord('q'):
+                break
 
-        cv.imshow("frame", edges)
-        key = cv.waitKey()
-
-        if key == 113:
-            quit()
+            # if key == 113:
+                # quit()
 
