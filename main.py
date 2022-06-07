79a8e2f2ea8756876d1010cafe44b7ab6c1de4d8
Nevin <nevinpuri1901@gmail.com>
2022-06-07 18:43:42 +0200

    added sobel

--- main.py
+++ main.py
@@ -43,18 +43,18 @@ if __name__ == "__main__":
 
         blur = cv.blur(combined, (5, 5))
 
-        edges = cv.Canny(combined, 100, 200)
+        edges = cv.Canny(blur, 100, 200)
 
         trapezoid = np.array([
         [
         # top left
-        (620-120, 520), # 494
+        (620-80, 520), # 494
         # bottom left
         (40, 918-120),
         # bottom right
         (1240-40, 918-120),
         # top right
-        (620+120, 520)]
+        (620+80, 520)] # 120
         ])
 
         mask = np.zeros_like(edges)
@@ -73,6 +73,11 @@ if __name__ == "__main__":
         t_matrix = cv.getPerspectiveTransform(np.float32(trapezoid[0]), desired_points)
         warped_frame = cv.warpPerspective(mask, t_matrix, (monitor["width"], monitor["height"]), flags=(cv.INTER_LINEAR))
 
+        grad_y = cv.Sobel(warped_frame, cv.CV_16S, 0, 1, ksize=3, scale=1, delta=0, borderType=cv.BORDER_DEFAULT)
+        abs_grad_y = cv.convertScaleAbs(grad_y)
+
+        print(grad_y)
+
         historigram = np.sum(warped_frame[int(warped_frame.shape[0]/2):,:], axis=0)
         plt.clf()
         plt.plot(historigram)
@@ -90,7 +95,7 @@ if __name__ == "__main__":
         non_zero_y = np.array(non_zero[0])
         non_zero_x = np.array(non_zero[1])
 
-        cv.imshow("frame", warped_frame)
+        cv.imshow("frame", abs_grad_y)
         if cv.waitKey(1) & 0xFF == ord('q'):
             break
 
